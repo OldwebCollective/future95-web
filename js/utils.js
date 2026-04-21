630646bd0c9c999637e89abb6e905b24574bbfcf
Mega Void <megavoid26478@fexpost.com>
2026-04-21 00:50:14 -0300

    feat: se eliminan console.log de posts.html y utils.js; se elimina validate-posts.js y directorio scripts

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,4 @@
 function formatDate(isoDate) {
-  console.log("Formatting date:", isoDate);
   const date = new Date(isoDate + 'T00:00:00Z');
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   return date.toLocaleDateString('en-US', options);
